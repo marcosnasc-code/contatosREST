7170a681f9edbdeac7a3f672374d410586da8007
marcosnasc-code <marcos.nacimento0321@gmail.com>
2025-02-18 01:09:36 -0300

    DTO - Sprint Security + Autenticação JWT - PARTE 4 - Método para geração do Token JWT + Método de validação Token

---
+++ contatos/src/main/java/br/com/fiap/contatos/config/security/token_service.java
@@ -0,0 +1,59 @@
+package br.com.fiap.contatos.config.security;
+
+
+import br.com.fiap.contatos.model.usuario_model;
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+@Service //classes de serviço precisam ser marcadas para o spring
+public class token_service {
+
+    @Value("${minhaChaveSecreta}") //A chave secreta é buscada fora da aplicação. Nesse caso
+    //ela ficará em application properties;
+    private String palavra_secreta;
+
+    public String gerarToken(usuario_model usuario){
+        try{
+            Algorithm algorithm = Algorithm.HMAC256(palavra_secreta);
+            String token = JWT
+                    .create()
+                    .withIssuer("Contatos") //Emissor do Token
+                    .withSubject(usuario.getEmail()) //login do usuario
+                    .withExpiresAt(gerarDataDeExpiracao()) //tempo de expiração do token
+                    .sign(algorithm); //construir algoritmo de acordo com o hash escolhido
+            return token;
+        } catch (JWTCreationException erro){
+            throw new RuntimeException("Não foi possível gerar o Token!");
+        }
+    }
+
+
+    public String validarToken(String token){
+        try{
+            Algorithm algorithm = Algorithm.HMAC256(token);
+            return JWT.require(algorithm)
+                    .withIssuer("Contatos")
+                    .build()
+                    .verify(token)
+                    .getSubject();
+        } catch (JWTVerificationException erro){
+            return "";
+        }
+    }
+
+
+    public Instant gerarDataDeExpiracao(){
+        return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
+        //retorna o horário.de agora.mais o adicional de X horas.de acordo com o fuso horário Y;
+    }
+
+
+}
